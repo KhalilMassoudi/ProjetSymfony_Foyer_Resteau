8c993071b929e6d24a1870adf47754d628e75f2b
KhalilMassoudi <khalil.massoudi@esprit.tn>
2024-11-13 16:23:32 +0100

    Initial commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('backtemplates/app-login.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('backtemplates/app_register.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/back', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('backtemplates/baseback.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/back2', name: 'app_index2')]
+    public function index2(): Response
+    {
+        return $this->render('backtemplates/baseback2.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/back/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('backtemplates/app_profile.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/back/calander', name: 'app_calander')]
+    public function calander(): Response
+    {
+        return $this->render('backtemplates/app_calander.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+    #[Route('/front', name: 'app_front')]
+    public function front(): Response
+    {
+        return $this->render('fronttemplates/basefront.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+}
---
+++ src/Entity/Service.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_creation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): static
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}
