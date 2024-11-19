fec850c8b692d3e0e23257bb258d73bb7ad15a0c
KhalilMassoudi <khalil.massoudi@esprit.tn>
2024-11-19 08:44:48 +0100

    Ajout de nouvelle fonctionnalité ajout service

---
+++ src/Form/ServiceFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Service;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ServiceFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('date_creation', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('date_fin', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Service::class,
+        ]);
+    }
+}
