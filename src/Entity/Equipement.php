e4070ba30ae1b2845e813b1f0a86cfb23bad7e5e
MohamedBechir21 <mohamedbechir.benattia@esprit.tn>
2024-11-21 22:02:54 +0100

    amélioration du front office

--- src/Entity/Equipement.php
+++ src/Entity/Equipement.php
@@ -18,6 +18,7 @@ class Equipement
     #[ORM\Column(type: 'string', length: 50)]
     private string $etatEquipementB;
 
+
     #[ORM\Column(type: 'date')]
     private \DateTimeInterface $dateDernierEntretienEquipementB;
 
---
+++ src/Repository/EquipementRepository.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Repository;
+
+use App\Entity\Equipement;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class EquipementRepository extends ServiceEntityRepository
+{
+public function __construct(ManagerRegistry $registry)
+{
+parent::__construct($registry, Equipement::class);
+}
+
+// Ajoutez des méthodes personnalisées si nécessaire
+}
