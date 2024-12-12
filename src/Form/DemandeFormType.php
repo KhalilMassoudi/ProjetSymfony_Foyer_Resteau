e17dfd1821d961fdff7bb7bd11c10f67cd529cab
KhalilMassoudi <khalil.massoudi@esprit.tn>
2024-12-12 22:48:50 +0100

    validation final -1

--- src/Form/DemandeFormType.php
+++ src/Form/DemandeFormType.php
@@ -5,19 +5,72 @@ namespace App\Form;
 use App\Entity\DemandeService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class DemandeFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('email')
-        ;
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le nom est obligatoire.']),
+                    new Assert\Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le nom ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le prénom est obligatoire.']),
+                    new Assert\Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le prénom ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le numéro de téléphone est obligatoire.']),
+                    new Assert\Regex([
+                        'pattern' => '/^\+?\d{10,15}$/',
+                        'message' => 'Le numéro de téléphone doit être valide.',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'L\'email est obligatoire.']),
+                    new Assert\Email(['message' => 'L\'email doit être valide.']),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description de la demande',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 4,
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La description est obligatoire.']),
+                    new Assert\Length([
+                        'max' => 500,
+                        'maxMessage' => 'La description ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
